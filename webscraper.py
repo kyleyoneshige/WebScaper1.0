c382981d852a1234766f790012da83214f99221b
Kyle Yoneshige <yoneshige.kyle@gmail.com>
2023-03-16 11:17:13 -0700

    First step of requesting geek data

--- webscraper.py
+++ webscraper.py
@@ -1,1 +1,6 @@
-print('hello world')
+#following guide: https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
+import requests
+
+URL = "https://www.geeksforgeeks.org/data-structures/"
+r = requests.get(URL)
+print(r.content)
