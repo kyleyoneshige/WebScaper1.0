27e9ae1fe7944c69dd1eb79135a310168ac2cf9c
Kyle Yoneshige <yoneshige.kyle@gmail.com>
2023-03-16 11:36:53 -0700

    Update to beautiful soup

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,11 @@
 #following guide: https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
 import requests
+from bs4 import BeautifulSoup
 
+headers = {'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"}
 URL = "https://www.geeksforgeeks.org/data-structures/"
-r = requests.get(URL)
-print(r.content)
+
+r = requests.get(url=URL, headers=headers)
+soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
+print(soup.prettify())
+
